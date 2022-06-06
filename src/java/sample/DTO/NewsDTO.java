a530ff4ef5edd5067ce31adfc5d433ae7b1a8e4c
An <100825165+nbNhatTan@users.noreply.github.com>
2022-06-06 15:58:48 +0700

    Create NewsDTO.java

---
+++ src/java/sample/DTO/NewsDTO.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sample.DTO;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author NhatTan
+ */
+public class NewsDTO {
+
+    private String newsID;
+    private String writerName;
+    private String title;
+    private String head;
+    private String Body;
+    private String AccountID;
+    private Date uploadDate;
+
+    public NewsDTO(String newsID, String writerName, String title, String head, String Body, String AccountID, Date uploadDate) {
+        this.newsID = newsID;
+        this.writerName = writerName;
+        this.title = title;
+        this.head = head;
+        this.Body = Body;
+        this.AccountID = AccountID;
+        this.uploadDate = uploadDate;
+    }
+
+    public String getNewsID() {
+        return newsID;
+    }
+
+    public void setNewsID(String newsID) {
+        this.newsID = newsID;
+    }
+
+    public String getWriterName() {
+        return writerName;
+    }
+
+    public void setWriterName(String writerName) {
+        this.writerName = writerName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getBody() {
+        return Body;
+    }
+
+    public void setBody(String Body) {
+        this.Body = Body;
+    }
+
+    public String getAccountID() {
+        return AccountID;
+    }
+
+    public void setAccountID(String AccountID) {
+        this.AccountID = AccountID;
+    }
+
+    public Date getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(Date uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    @Override
+    public String toString() {
+        return "FPTLibrary.sample.DTO.Account[ id=" + newsID + " ]";
+    }
+}
