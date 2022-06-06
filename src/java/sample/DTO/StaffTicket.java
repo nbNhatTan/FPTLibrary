f3e798ee808f19d0dfd5d7846e24e256d43d8f8a
An <100825165+nbNhatTan@users.noreply.github.com>
2022-06-06 15:59:09 +0700

    Create StaffTicket.java

---
+++ src/java/sample/DTO/StaffTicket.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sample.DTO;
+
+/**
+ *
+ * @author NhatTan
+ */
+public class StaffTicket {
+
+    private String staffID;
+    private String ticketID;
+
+    public StaffTicket(String staffID, String ticketID) {
+        this.staffID = staffID;
+        this.ticketID = ticketID;
+    }
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
+
+}
