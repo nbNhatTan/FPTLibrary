d2ed4b31e7eb87bd6a9ade03a491a27cacdca477
An <100825165+nbNhatTan@users.noreply.github.com>
2022-06-06 15:58:05 +0700

    Create AuthorDTO.java

---
+++ src/java/sample/DTO/AuthorDTO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sample.DTO;
+
+/**
+ *
+ * @author NhatTan
+ */
+public class AuthorDTO {
+
+    private int authorID;
+    private String authorName;
+
+    public AuthorDTO(int authorID, String authorName) {
+        this.authorID = authorID;
+        this.authorName = authorName;
+    }
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+}
